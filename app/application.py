4eade13662693daae7e725b00dfc16adefc49b0f
ruangfafa <ruangfafa@outlook.com>
2025-11-23 13:39:30 -0500

    [optimize]Organize the class format

--- app/application.py
+++ app/application.py
@@ -1,12 +1,10 @@
 from app.common.constant import ApplicationCons
-from app.common.enum import Request
-from app.service.authorization_service import authorization
 from app.service.logging_service import get_logger
 
 logger = get_logger(ApplicationCons.LOGGER)
 
 def main(args=None):
-    authorization(Request.AS , "c01176a0-158b-40b8-8712-a8faf89f0cc4")
+    pass
 
 if __name__ == ApplicationCons.MAIN_MODULE:
     main()
--- app/common/constant.py
+++ app/common/constant.py
@@ -20,19 +20,15 @@ class MysqlServiceCons:
     SALT = "salt_hex"
     HASH = "hash_hex"
 
-class AuthorizationServiceCons:
+class SecurityServiceCons:
     LOGGER = "authorization_service"
 
     L_PERM_ALLOW_1 = "Permission granted normally for request %s uuid=%s"
     L_PERM_ALLOW_2 = "Permission granted by time condition at %s for request %s uuid=%s"
     L_PERM_DENIED_2 = "Permission denied by time window at %s for request %s uuid=%s"
     L_PERM_DENIED = "Permission DENIED for request %s role=%s uuid=%s"
-
-    TIME_FORMAT = "%H:%M"
-
-class AccountManagerServiceCons:
-    LOGGER = "account_manager_service"
-
     L_SUC_LOGIN = "Login success for user: %s"
     L_FAIL_LOGIN = "Login failed for user: %s, Reason: Wrong Password"
     L_FAIL_LOGIN_2 = "Login failed for user: %s, Reason: Something went wrong[%s]"
+
+    TIME_FORMAT = "%H:%M"
