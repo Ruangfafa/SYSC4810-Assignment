50323ceacbbbfef8be24d725f67b9a3b22c6499c
ruangfafa <ruangfafa@outlook.com>
2025-11-26 21:44:05 -0500

    [feature]Complete rest of the stuff

--- app/application.py
+++ app/application.py
@@ -1,10 +1,11 @@
 from app.common.constant import ApplicationCons
 from app.service.logging_service import get_logger
+from app.view.system_view import system_view
 
 logger = get_logger(ApplicationCons.LOGGER)
 
-def main(args=None):
-    pass
+def main():
+    system_view()
 
 if __name__ == ApplicationCons.MAIN_MODULE:
     main()
--- app/common/enum.py
+++ app/common/enum.py
@@ -7,6 +7,14 @@ class Role(Enum):
     FP = "FP" #financial planner
     T = "T" #teller
 
+ROLE_LABELS = {
+    Role.C: "Client",
+    Role.PC: "Premium Client",
+    Role.FA: "Financial Advisor",
+    Role.FP: "Financial Planner",
+    Role.T: "Teller",
+}
+
 
 class Request(Enum):
     VAB = 0   # view account balance
@@ -17,3 +25,14 @@ class Request(Enum):
     VMI = 5   # view money market instrument
     VPI = 6   # view private consumer instrument
     AS  = 7   # access system
+
+REQUEST_LABELS = {
+    Request.VAB: "view account balance",
+    Request.VIP: "view investment portfolio",
+    Request.MIP: "modify investment portfolio",
+    Request.VFA: "view contact detail of FA",
+    Request.VFP: "view contact detail of FP",
+    Request.VMI: "view money market instrument",
+    Request.VPI: "view private consumer instrument",
+    Request.AS:  "access system",
+}
